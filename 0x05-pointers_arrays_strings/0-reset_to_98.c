e2e07ebcef51848e1803350b09fa4d13bad30216
Nazavivian21 <nazafavourukwe@gmail.com>
2024-01-02 18:58:54 -0800

    Hi Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rest_to_98 -  a function name
+ * Description: updates an integer value
+ * @n:  a pointer variable
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
