23af8a3a350066db265d1af529fab529722f12d7
Nazavivian21 <nazafavourukwe@gmail.com>
2024-01-04 06:35:24 -0800

    Hi Pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * swap_int - a funtion
+ * Description: It swaps the value of two integers
+ * @a: a pointer to an integer
+ * @b: a pointer to an integer
+ * Return: Nothing
+ */
 void swap_int(int *a, int *b)
 {
 	int c;
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
+/**
+ * _strlen - a function name
+ * Description: It checks the length of a string
+ * @s: A pointer variable
+ * Return: An integer
+ */
 int _strlen(char *s)
 {
 	int string_len;
 
-	if (string_len = 0)
+	string_len = 0;
+	while (*s != '\0')
 	{
+		s++;
 		string_len++;
 	}
-	else
-	{
-		return (string_len);
-	}
+	return (string_len);
 }
