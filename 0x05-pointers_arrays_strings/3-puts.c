ee02fe0a7ee9b24d693b370eb43431107829ddfe
Nazavivian21 <nazafavourukwe@gmail.com>
2024-01-05 06:55:33 -0800

    Hi Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+ * _puts - a function name
+ * Description: a function that prints a strings
+ * @str: a pointer to a string
+ * Return: a string
+ */
 
 void _puts(char *str)
 {
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int len, i, j;
+	char var;
+
+	for (len = 0; s[length] != '\0'; len++)
+	{
+		i = 0;
+		j = len / 2;
+	}
+
+	while (j--)
+	{
+		var = s[len - i - 
