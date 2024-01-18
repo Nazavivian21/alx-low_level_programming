af602869abf7e9308397af7318699640385ceecb
Nazavivian21 <nazafavourukwe@gmail.com>
2024-01-18 07:43:24 -0800

    Functions in C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -46,3 +46,39 @@ void print_times_table(int n)
 		}
 	}
 }
+/**
+ * print_double - a function that prints numbers
+ * Description: It prints double numbers
+ * @num: an integer that stores numbers
+ * Return: void
+ */
+void print_double(int num)
+{
+	int left, right;
+
+	_putchar(' ');
+	_putchar(' ');
+	left = num / 10;
+	right = num % 10;
+	_putchar('0' + left);
+	_putchar('0' + right);
+}
+/**
+ * print_tripple - a function that prints numbers
+ * Decription: It prints tripple numbers
+ * @res: an integer that stores values
+ * Return: void
+ */
+void print_tripple(int res)
+{
+	int left, l_left, r_left, right;
+
+	_putchar(' ');
+	left = res / 10;
+	l_left = left / 10;
+	r_left = left % 10;
+	right = res % 10;
+	_putchar('0' + l_left);
+	_putchar('0' + r_left);
+	_putchar('0' + right);
+}
