7dec7c3ffe56e7c554a347efcaf4f52cbeed163f
Nazavivian21 <nazafavourukwe@gmail.com>
2024-01-23 23:01:41 -0800

    Preprocessors in C

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s;
+
+	i = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
