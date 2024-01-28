cc7c4aff40cc31ff6ded7bee5c1757c26e35c297
Nazavivian21 <nazafavourukwe@gmail.com>
2024-01-28 15:59:21 -0800

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void pointer_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_day(int month, int day, it year);
+void positive_or_negative(int i);
 
-#endif
+#endif /* MAIN_H */
