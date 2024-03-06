0c4a42eb10963fbc2d9eaff3e5af6e7431893541
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-03-06 11:59:33 +0000

    prints most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 58; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 50)
+		if (i != '2')
 		{
-			if (i != 52)
+			if (i != '4')
 			{
 				_putchar (i);
 			}
