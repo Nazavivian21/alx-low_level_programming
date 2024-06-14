ce385fc63b2377f705fc03676ef9fb0013ea444a
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-14 05:58:54 +0100

    Make new corrections to the file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,18 +7,22 @@
 */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	for (int i = 0; i < n; i++)
+	else
 	{
-		for (int j = 0; j < i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar (' ');
+			}
+			_putchar ('\\');
+			_putchar ('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
 	}
 }
