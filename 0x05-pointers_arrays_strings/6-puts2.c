72fb3c5fba8c157f4ad0fbc22c24e82b28c3d764
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-13 22:30:54 +0100

    Pass a betty check

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,12 +2,13 @@
 /**
  * puts2 - prints every other character of a string
  * Description: this function prints out every other character in a string
+ * @str: the string to print from.
  * Return: void
  */
 void puts2(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
