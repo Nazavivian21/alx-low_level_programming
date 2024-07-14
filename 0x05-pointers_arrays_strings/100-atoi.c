75583d0dd9146f522aa22dbb68866e62fcd78d82
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-14 20:33:24 +0100

    Pass a betty check

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,42 +7,41 @@
  * Return: an integer
  */
 
-int _atoi(char *s) {
-    int result;
-    int sign;
+int _atoi(char *s)
+{
+	int result;
+	int sign;
 	int digit;
-    bool start_num;
+	bool start_num;
 
 	result = 0;
 	sign = 1;
 	start_num = false;
 
-    while (*s) 
+	while (*s)
 	{
-        if (*s == '-')
+		if (*s == '-')
 		{
-            sign = -sign;
-        } 
-		else if (*s == '+') 
+			sign = -sign;
+		}
+		else if (*s == '+')
 		{
-          ;  
-        } 
-		else if (*s >= '0' && *s <= '9') 
+			;
+		}
+		else if (*s >= '0' && *s <= '9')
 		{
-            start_num = true;
-    			digit = *s - '0';
-            if (result > (2147483647 - digit) / 10) 
+			start_num = true;
+				digit = *s - '0';
+			if (result > (2147483647 - digit) / 10)
 			{
-                return (sign == 1) ? 2147483647 : -2147483648;
-            }
-            result = result * 10 + digit;
-        } 
+				return ((sign == 1) ? 2147483647 : -2147483648);
+			}
+			result = result * 10 + digit;
+		}
 		else if (start_num)
 		{
-            break; 
-        }
-        s++;
-    }
-
-    return sign * result;
-}
+			break;
+		}
+		s++;
+	}
+	return (sign * result);
