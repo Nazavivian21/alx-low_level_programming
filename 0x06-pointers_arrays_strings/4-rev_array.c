de6537c25480569477c1e00d5806b0e50d5947f2
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-17 03:06:46 +0100

    Reverse a string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int temp, i;
 
-	n = n - 1;
-	j = 0;
-	while (j <= 0)
+	for (i = 0; i < n / 2; i++)
 	{
-		i = a[j];
-		a[j++] = a[n];
-		a[n--] = i;
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }
