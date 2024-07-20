43330378f8c6e9bbe3bc9307efd97ec3c165c40c
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-20 09:28:17 +0100

    Pass a new betty check

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,14 +3,14 @@
 
 /**
  * linear_search - searches for a value in an array of integers.
- * Description: searches for a value in an array of integers using the Linear \
- * search algorithm
  * @array: a pointer to the first element of the array to search in.
- * @size: is the number of elements in array.
- * @value:  is the value to search for.
- * Return: an integer.
+ * @size: is the number of elements in the array.
+ * @value: is the value to search for.
+ * Description: searches for a value in an array of integers using
+ * the Linear search algorithm.
+ * Return: the first index where value is located, or -1 if value
+ * is not present or if array is NULL.
  */
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
