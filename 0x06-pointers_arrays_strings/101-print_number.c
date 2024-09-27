101d02693119dee0dc5654e31bf2e2e65a923d12
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-28 00:35:50 +0100

    Magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,15 +4,17 @@
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');         
-        n = -n;                
-    }
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    if (n / 10) {               
-        print_number(n / 10); 
-    }
-    
-    _putchar((n % 10) + '0');  
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
