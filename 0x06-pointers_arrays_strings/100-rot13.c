04e26b6c3630f65eedb8d2ce9dc010c7b9e09b71
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-28 00:20:39 +0100

    Magic

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to be encoded.
+ *
+ * Return: The encoded string.
+ */
+char *rot13(char *str) {
+    char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char *encoded = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    int i, j;
+
+    for (i = 0; str[i] != '\0'; i++) {
+        for (j = 0; original[j] != '\0'; j++) {
+            if (str[i] == original[j]) {
+                str[i] = encoded[j];
+                break;  // Exit the inner loop if a match is found
+            }
+        }
+    }
+    return str;  // Return the encoded string
+}
